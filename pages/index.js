4ecc035d0b99e01c83433f45cd53d846b39b46a0
Jesus <je.flores28@gmail.com>
2022-01-26 23:29:43 -0600

    fetching cms data

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,39 @@
-export default function Home() {
+import { gql, GraphQLClient } from "graphql-request";
+
+export const getStaticProps = async () => {
+  const url = process.env.ENDPOINT;
+
+  const GQLClient = new GraphQLClient(url, {
+    headers: {
+      Authorization: process.env.NEXT_PUBLIC_DISNEY_TOKEN
+    }
+  });
+
+  const query = gql`
+    query {
+      videos {
+        id
+        title
+        description
+        seen
+        slug
+        tags
+      }
+    }
+  `;
+
+  const data = await GQLClient.request(query);
+  const videos = data.videos;
+  return {
+    props: {
+      videos
+    }
+  };
+};
+
+const Home = ({ videos }) => {
+  console.log(videos);
   return <div>Hello</div>;
-}
+};
+
+export default Home;
