1ad0b7fb9b0672bd0c470d2e3110d40dd16d2676
Jesus <je.flores28@gmail.com>
2022-01-29 18:54:01 -0600

    filtering videos and genre

---
+++ components/Section.js
@@ -0,0 +1,18 @@
+import Card from "./Card";
+
+const Section = ({ genre, videos }) => {
+  return (
+    <div className={"section"}>
+      <h3>{genre}</h3>
+      <div className="video-feed">
+        {videos?.map(v => (
+          <a key={v.id} href={`/video/${v.slug}`}>
+            <Card thumbnail={v.thumbnail} />
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Section;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { gql, GraphQLClient } from "graphql-request";
+import Section from "../components/Section";
 
 export const getStaticProps = async () => {
   const url = process.env.ENDPOINT;
@@ -18,6 +19,9 @@ export const getStaticProps = async () => {
         seen
         slug
         tags
+        thumbnail {
+          url
+        }
       }
     }
   `;
@@ -32,8 +36,45 @@ export const getStaticProps = async () => {
 };
 
 const Home = ({ videos }) => {
-  console.log(videos);
-  return <div>Hello</div>;
+  const randomVideo = videos => {
+    return videos[Math.floor(Math.random() * videos.length)];
+  };
+
+  const filterVideos = (videos, genre) => {
+    return videos.filter(video => video.tags.includes(genre));
+  };
+
+  const unSeenVideos = videos => {
+    return videos.filter(video => video.seen == false || videos.seen == null);
+  };
+
+  return (
+    <>
+      <div className="app">
+        <div className="main-video">
+          <img
+            src={randomVideo(videos).thumbnail.url}
+            alt={randomVideo(videos).title}
+          />
+        </div>
+
+        <div className="video-feed">
+          <Section
+            genre={"Recommended for you"}
+            videos={unSeenVideos(videos)}
+          />
+          <Section genre={"action"} videos={filterVideos(videos, "family")} />
+          <Section genre={"comedy"} videos={filterVideos(videos, "comedy")} />
+          <Section genre={"family"} videos={filterVideos(videos, "family")} />
+          <Section genre={"fantasy"} videos={filterVideos(videos, "fantasy")} />
+          <Section
+            genre={"superhero"}
+            videos={filterVideos(videos, "superhero")}
+          />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
